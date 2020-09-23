97966a1bb524a5e2cc85c2674e2c59368216ef72
Chris Hundhausen <hundhaus@wsu.edu>
2020-09-22 22:28:34 -0700

    Added addStation()

--- js/weatherobs.js
+++ js/weatherobs.js
@@ -1,11 +1,11 @@
 class WeatherStation extends React.Component {
     
     constructor(props) {
-      super(props);
-      this.state = {latitude: this.props.latitude,
-                    longitude: this.props.longitude
-                   };
-    }
+        super(props);
+        this.state = {latitude: this.props.latitude,
+                      longitude: this.props.longitude,
+                     };
+      }
 
     componentDidMount = () => {
         this.getCurrentObservations();
@@ -66,7 +66,8 @@ class WeatherStation extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {station: null};
+        this.state = {stations: [],
+                      stationCount: 0};
       }
 
     componentDidMount = () => {
@@ -77,33 +78,65 @@ class WeatherStation extends React.Component {
      //Called when user agrees to give loc data. We set the first weather
      //station to show conditions at the user's current position.
     getLocSuccess = (position) =>  {
-         this.setState({station: {lat: position.coords.latitude, long: position.coords.longitude}});
+        this.setState({stations: [{lat: position.coords.latitude, 
+            lon: position.coords.longitude, 
+            stationId: this.state.stationCount+1}],
+            stationCount: this.state.stationCount + 1});    
     }
   
     //Called when user refuses to give access to loc data
     //Initializes first weather station to Seattle in this case
     getLocError = (err) => {
-         this.setState({station: {lat: 47.61, long: -122.33}});
-    }
+        this.setState({stations: [{lat: 47.61, 
+                                   lon: -122.33, 
+                                   stationId: this.state.stationCount+1}],
+                       stationCount: this.state.stationCount + 1});
+   }
+
+   //addStation -- When user clicks on "+" button to add a new weather station,
+    //prompt the user for the location and attempt to add the requested station.
+    addStation = async() => {
+        const newStation = prompt("Enter a City, State, and Country:");
+        if (newStation != null) { //Need to see if we can find the station through the API 
+          const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + 
+              newStation +  '&appid=98cb8d2538da248784d8e1c1f9332ea9');
+          const stationData = await response.json();
+          //See if the requested station exists
+          if (stationData != null && stationData.hasOwnProperty('coord')) { 
+              //Push new station into stations list
+              let newStations = [...this.state.stations];
+              newStations.push({lat: stationData.coord.lat, 
+                                lon: stationData.coord.lon, 
+                                stationId: this.state.stationCount + 1});
+              this.setState({stations: newStations,
+                             stationCount: this.state.stationCount + 1});
+          } else { 
+              alert("Sorry, that weather location could not be found.");
+          }
+        }
+      }
   
     render() {
-        if (this.state.station != null) {
-            return (
-                <div id="main">
-                    <div align="center" className="jumbotron">
-                    <WeatherStation latitude={this.state.station.lat} longitude={this.state.station.long} />
-                    </div>
-                    <div className="floatButton" id="floatBtnDiv">
-                        <a className="float" id="addStationBtn" onClick={this.addStation}>
-                            <span className="float-btn-icon fa fa-plus" id="floatBtnIcon"></span>
-                        </a>
-                    </div>  
-                </div>
-                
-            );
-        } else {
-            return null;
+        let rows = [];
+        for (let i = 0; i < this.state.stations.length; ++i) {
+            rows.push(<WeatherStation key={this.state.stations[i].stationId} 
+                    latitude={this.state.stations[i].lat} 
+                    longitude={this.state.stations[i].lon}
+                    stationId={this.state.stations[i].stationId} />);
         }
+        return (
+            <div id="main">
+                <div id="weatherStations">
+                    {rows}
+                </div>
+                <div className="floatButton" id="floatBtnDiv">
+                    <a className="float" id="addStationBtn" onClick={this.addStation}>
+                        <span className="float-btn-icon fa fa-plus" id="floatBtnIcon"></span>
+                    </a>
+                </div>  
+            </div>
+            
+        );
     }
   }
   
